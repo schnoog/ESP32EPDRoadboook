792d79179eb6be5a8a8c1eea1009ed6cf4a3b7f7
schnoog <braun.volker@gmail.com>
2018-07-25 11:25:57 +0200

    still working ...with EPD

--- src/include_ReadAndShow.h
+++ src/include_ReadAndShow.h
@@ -2,12 +2,27 @@
 #include <cstdlib>
 #include <rBase64.h>
 
-
-
 void ShowEntries(char *filename, int intStartWith , int MaxCount){
     String SCC = sd_getfilecontent(filename);
+//    const uint8_t *f = u8g2_font_courB24_tf;
+    const uint8_t *f = u8g2_font_fub42_tf;
+    const int16_t yOS = 50;
+    const int16_t LeftSetOff = 20;
+
+    display.setRotation(0);
+    display.fillScreen(GxEPD_WHITE);
+    u8g2_for_adafruit_gfx.setFontMode(1);                   // use u8g2 transparent mode (this is default)
+    u8g2_for_adafruit_gfx.setFontDirection(0);              // left to right (this is default)
+    u8g2_for_adafruit_gfx.setForegroundColor(GxEPD_BLACK);  // apply Adafruit GFX color
+    u8g2_for_adafruit_gfx.setBackgroundColor(GxEPD_WHITE);  // apply Adafruit GFX color
+    u8g2_for_adafruit_gfx.setFont(f); // select u8g2 font from here: https://github.com/olikraus/u8g2/wiki/fntlistall
+
+    //u8g2_for_adafruit_gfx.println("Umlaut ÄÖÜäéöü");
+  
+    int16_t EffTopOffSet = 0;
     int Mcount = 0;
     int retCount = 0;
+    int OSC = 0;
 //    StaticJsonBuffer<1024> jsonBuffer;
     DynamicJsonBuffer jsonBuffer;
     //Serial.println(SCC);
@@ -18,6 +33,7 @@ void ShowEntries(char *filename, int intStartWith , int MaxCount){
             //Serial.println("Eintrag " + (String)count);
             if (Mcount >= intStartWith){
                 if(retCount < MaxCount){
+                    OSC++;
                     //Serial.println("LoopOutput") ;
                     String SID = (*it)["id"];
                     String PName = (*it)["name"];
@@ -33,11 +49,16 @@ void ShowEntries(char *filename, int intStartWith , int MaxCount){
                     Serial.println(PName);
                     Serial.println(SID +  "  " + PDone);
                     Serial.println("---------------------------------");
+                    EffTopOffSet = (int16_t)OSC * yOS;
+                    u8g2_for_adafruit_gfx.setCursor(LeftSetOff, EffTopOffSet);
+                    u8g2_for_adafruit_gfx.print(PName);
                     retCount++;
                 }
             }
             Mcount++;
         }
+    display.update();
+        
 Serial.println("Count:" + (String)Mcount);
 Serial.println("RetCount:" + (String)retCount);
 }
